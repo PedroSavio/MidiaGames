e83c2359c7a7ac133dccdf5f99df607b3b916be4
PedroSavio <saviokbca@gmail.com>
2023-07-10 22:17:33 -0300

    Ronnie Coliman

---
+++ src/main/java/com/saviomidia/initProj/InitProjApplication.java
@@ -0,0 +1,13 @@
+package com.saviomidia.initProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saviomidia/initProj/InitProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saviomidia.initProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
